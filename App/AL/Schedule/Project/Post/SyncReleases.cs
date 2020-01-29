905756cf3fc19ee886e2ab9b6919b46bf7a9c5f9
Max Kovalenko <mxsshere@gmail.com>
2020-01-29 22:18:39 +0300

    sync releases date hotfix

--- App/AL/Schedule/Project/Post/SyncReleases.cs
+++ App/AL/Schedule/Project/Post/SyncReleases.cs
@@ -55,7 +55,9 @@ namespace App.AL.Schedule.Project.Post {
                                         repo.Project(), $"Released {release.Name}", release.Body
                                     );
                                     post.UpdateCol("origin_id", release.Id.ToString());
-                                    post.UpdateCol("created_at", release.PublishedAt.ToString());
+                                    post.UpdateCol(
+                                        "created_at", release.PublishedAt.Value.ToUnixTimeSeconds().ToString()
+                                    );
                                 }
                             }
                             catch (Exception e) {
